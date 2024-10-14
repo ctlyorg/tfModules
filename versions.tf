3a2df607e919c5b3b5747048517aa362dbc5e491
Clement Tan <clementtly@gmail.com>
2024-10-14 13:41:30 +0800

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
